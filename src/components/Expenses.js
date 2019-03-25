832ed0a5f5b36fe5411fea87c547950555c4f7e2
Roxolana <roxolanapavlyk@gmail.com>
2019-03-25 23:12:43 +0100

    diplay list expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import axios from 'axios'
 
-import useExpensesList from './useExpensesList';
+import ExpenseOne from './ExpenseOne';
+import useExpensesList from '../containers/useExpensesList';
 
 export default function Expenses() {
     const expenses = useExpensesList()
+    function onDelete(id) {
+        console.log(id)
+        axios.delete(`http://127.0.0.1:8000/expenses/${id}/`)
+    }
+    
   return (
     <div>
-      {expenses.map(expense=>(
-          <ul key={expense.id}><li>{expense.title}</li></ul>
+      {expenses.map((expense,index)=>(
+              <ExpenseOne key={index} title={expense.title} index={index} id={expense.id} onDelete={onDelete}/>
       ))}
     </div>
   )
