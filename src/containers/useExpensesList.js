006ef8104231fc4766d32f69da65e639ae3f361f
Roxolana <roxolanapavlyk@gmail.com>
2019-03-25 23:15:14 +0100

    containers added

---
+++ src/containers/useExpensesList.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+
+export default function useExpensesList() {
+    const [expenses, setExpenses] = useState([])
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/expenses/')
+            .then(res => setExpenses(res.data))
+    })
+  return expenses
+}
