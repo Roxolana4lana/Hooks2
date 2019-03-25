bfc573b01edf81cca04f7daaa1ec7b470271e208
Roxolana <roxolanapavlyk@gmail.com>
2019-03-25 23:10:44 +0100

    dispaly form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import '../styles/App.scss'
+import Expenses from './Expenses';
+import ExpenseForm from './ExpenseForm';
 
 
 export default class App extends Component {
@@ -7,7 +9,8 @@ export default class App extends Component {
         return (
             <div className='App'>
                 <h1>My react app</h1>
-                <p>test</p>
+                <ExpenseForm/>
+                <Expenses/>
             </div>
         )
     }
