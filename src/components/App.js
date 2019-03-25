ccc4785318b698d2ada5860ce5f60e16124f52a7
Roxolana <roxolanapavlyk@gmail.com>
2019-03-25 23:46:13 +0100

    added browserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import '../styles/App.scss'
 import Expenses from './Expenses';
-import ExpenseForm from './ExpenseForm';
+import {Route, BrowserRouter} from 'react-router-dom'
 
 
 export default class App extends Component {
     render() {
         return (
+            <BrowserRouter>
             <div className='App'>
-                <h1>My react app</h1>
-                <ExpenseForm/>
-                <Expenses/>
+               <Route exact path='/' component={Expenses}/>
+               
             </div>
+            </BrowserRouter>
         )
     }
 }
