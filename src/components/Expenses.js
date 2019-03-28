7a0771abd34b34f6e985c9166aa17277958e76ce
Roxolana <roxolanapavlyk@gmail.com>
2019-03-28 15:37:03 +0100

    clean up

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-
+import ExpenseForm from './ExpenseForm';
 import ExpenseOne from './ExpenseOne';
 import useExpensesList from '../containers/useExpensesList';
 
+
 export default function Expenses() {
+
     const expenses = useExpensesList()
     function onDelete(id) {
         console.log(id)
@@ -12,13 +14,20 @@ export default function Expenses() {
             .catch(error => {
                 throw (error)
             })
+
     }
-    
+   
   return (
-    <div>
+      <div className='App'>
+          <div className="split expenses_lising">
+    
       {expenses.map((expense,index)=>(
               <ExpenseOne key={index} title={expense.title} index={index} id={expense.id} onDelete={onDelete}/>
       ))}
+      </div>
+          <div className="split App_Form">
+          <ExpenseForm />
+    </div>
     </div>
   )
 }
